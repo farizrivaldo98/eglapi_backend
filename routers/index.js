4340952d9545033b2fb7c54b8da3a688fbca646b
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2026-06-29 10:57:29 +0700

    update typo

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,6 @@
 // routers/index.js
-const databaseRouter = require("./databaseRouter");
+const databaseRouter = require("./databasaeRouter");  // ← sesuai nama file asli (ada typo 'ae')
 const auditRouter    = require("./auditRouter");
-const adminRouter    = require("./adminRouter");   // ← TAMBAHAN
+const adminRouter    = require("./adminRouter");       // ← yang baru
 
 module.exports = { databaseRouter, auditRouter, adminRouter };
