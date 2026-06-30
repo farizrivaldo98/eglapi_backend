13fc05de0faf3243a63f1cb84334a9c1610b116b
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2026-06-30 11:17:25 +0700

    revisi endpoint

--- routers/databasaeRouter.js
+++ routers/databasaeRouter.js
@@ -35,7 +35,7 @@ routers.get("/getTempChart", databaseControllers.getTempChart);
 routers.get("/getAllDataEMS", databaseControllers.getAllDataEMS);
 
 // Tambahkan di bawah kumpulan router lainnya
-routers.get("/admin/page-access",veryfyToken,databaseControllers.getPageAccess);
-routers.put("/admin/page-access",veryfyToken,checkRole,databaseControllers.updatePageAccess);
+routers.get("/page-access",veryfyToken,databaseControllers.getPageAccess);
+routers.put("/page-access",veryfyToken,checkRole,databaseControllers.updatePageAccess);
 
 module.exports = routers;
