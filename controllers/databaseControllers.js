b97c06533c0d18b53e572423fa33f61eb888997d
IND2026 <sidharmv07@gmail.com>
2026-08-12 18:09:33 +0700

    update cMT-C21B_SDP1-OFC23_data

--- controllers/databaseControllers.js
+++ controllers/databaseControllers.js
@@ -97,7 +97,9 @@ const ENERGY_POWER_TABLES = {
   lapi1: "cMT-C21B_PP_LAPI1_data",
   SDP2_Pro1:"cMT-C21B_SDP2_Pro1_data",
   SDP1_Ofc1:"cMT-C21B_SDP1_OFC1_data",
-  PP_Chiller:"cMT-C21B_PP_Chiller_data"
+  PP_Chiller:"cMT-C21B_PP_Chiller_data",
+  SDP1-OFC23 : "cMT-C21B_SDP1-OFC23_data"
+  
 };
 const ENERGY_POWER_PERIODS = ["hourly", "daily", "monthly"];
   
